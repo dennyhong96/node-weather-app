0c356c13b3113da6253b3c3f3c174af586c07b75
hhy666 <hong961127@gmail.com>
2020-05-06 21:05:11 -0700

    remove console logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-// console.log("Client side JavaScript file is loaded!");
-
 const weatherForm = document.querySelector("form");
 const address = document.querySelector("input");
 const msg1 = document.querySelector("#msg1");
